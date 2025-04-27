2df0dcb91f7a22e7ca445bfc7dd781d26f7a5556
gywns <gywns8339@naver.com>
2025-04-27 13:46:01 +0900

    숙제

--- js/condition6.js
+++ js/condition6.js
@@ -22,6 +22,9 @@ if (num1 > num2) {
 let result1 = 40 + Math.floor(Math.random() * 31);
 let result2 = 40 + Math.floor(Math.random() * 31);
 
+console.log(result1);
+console.log(result2);
+
 if (result1 > result2) {
   console.log("두 숫자 중에서 큰 값은 " + result1 + " 입니다.");
 } else if (result2 > result1) {
